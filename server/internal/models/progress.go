de96f2bbc66a7b98e8771d7741812ae4404c7079
zyh6663 <1134463893@qq.com>
2026-05-04 12:30:23 +0800

    fix: remove duplicate UpdatedAt field in Progress model

--- server/internal/models/progress.go
+++ server/internal/models/progress.go
@@ -1,16 +1,13 @@
 package models
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type Progress struct {
 	gorm.Model
-	BookID       uint      `json:"book_id" gorm:"index;not null"`
-	UserID       string    `json:"user_id" gorm:"index;default:''"`
-	ChapterIndex int       `json:"chapter_index" gorm:"default:0"`
-	Position     float64   `json:"position" gorm:"default:0"`
-	UpdatedAt    time.Time `json:"updated_at"`
+	BookID       uint   `json:"book_id" gorm:"index;not null"`
+	UserID       string `json:"user_id" gorm:"index;default:''"`
+	ChapterIndex int    `json:"chapter_index" gorm:"default:0"`
+	Position     float64 `json:"position" gorm:"default:0"`
 }
