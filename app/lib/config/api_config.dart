f8e68e148945486a14a8d79fb940f652d47d360d
purereader <purereader@example.com>
2026-05-17 11:44:14 +0800

    z'y'hzyh

--- app/lib/config/api_config.dart
+++ app/lib/config/api_config.dart
@@ -1,5 +1,5 @@
 ﻿class ApiConfig {
   // 云机本地运行：后端和前端在同一台机器，127.0.0.1 或 localhost 都可以
   // 如果是 Android 模拟器需要用 10.0.2.2
-  static const String baseUrl = 'http://127.0.0.1:8080';
+  static const String baseUrl = 'http://38.76.213.204:8080';
 }
